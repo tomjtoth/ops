390f19f7e58db5968915f2d8b114a1c39c42e961
Tamás Tóth <tomjtoth@gmail.com>
2024-11-19 11:11:32 +0200

    more readable battery-monitor

--- bin/battery-monitor.sh
+++ bin/battery-monitor.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 
-if [[ ! -v CRIT_BAT_LEVEL  ||  ! -v CRIT_BAT_COMMAND  ||  ! -v PATH_TO_BAT ]]
+if [[
+	! -v CRIT_BAT_LEVEL  ||
+	! -v CRIT_BAT_COMMAND  ||
+	! -v PATH_TO_BAT
+]]
 then
 	echo "example usage:
 	PATH_TO_BAT=/sys/class/power_supply/BAT0 \\
@@ -12,10 +16,14 @@ then
 fi
 
 while true; do
-	CURR=($(cat "${PATH_TO_BAT}"/{capacity,status}))
+	STATE=($(cat "${PATH_TO_BAT}"/{capacity,status}))
 
-	if [[ ${CURR[0]} -le $CRIT_BAT_LEVEL && "${CURR[1]}" != "Charging" ]]; then
-		echo "${CURR[1]} at ${CURR[0]}% executing: \"$CRIT_BAT_COMMAND\""
+	if [[ 
+		${STATE[0]} -le $CRIT_BAT_LEVEL &&
+		"${STATE[1]}" != "Charging"
+	]]
+	then
+		echo "${STATE[1]} at ${STATE[0]}% => \"$CRIT_BAT_COMMAND\""
 		$CRIT_BAT_COMMAND
 	fi
 
