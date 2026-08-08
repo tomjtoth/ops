2ed10507d0b7c2a7822fdff3c53e2f3d28422207
Tamás Tóth <tomjtoth@gmail.com>
2026-08-08 14:16:35 +0300

    added launchers for avp games

---
+++ roles/home/files/.local/bin/avp2010.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export WINEPREFIX=~/.wine-avp2010
+
+cd "/home/ttj/.wine-avp2010/drive_c/Program Files (x86)/DODI-Repacks/Aliens vs Predator"
+
+wine AvP_Launcher.exe
---
+++ roles/home/files/.local/bin/avp2primal-hunt.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export WINEPREFIX=~/.wine 
+
+cd "/home/ttj/.wine/drive_c/Program Files (x86)/Aliens versus Predator 2 - Primal Hunt"
+
+wine "PrimalHunt!.exe"
