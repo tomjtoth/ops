dfa8877b29ce69e9ec6aa634b4d9558348b5261b
Tamás Tóth <tomjtoth@gmail.com>
2026-08-15 12:41:09 +0300

    unified game launching via wine.sh

--- roles/home/files/.local/bin/avp2primal-hunt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export WINEPREFIX=~/.wine 
-
-cd "/home/ttj/.wine/drive_c/Program Files (x86)/Aliens versus Predator 2 - Primal Hunt"
-
-wine "PrimalHunt!.exe"
---
+++ roles/home/files/.local/bin/wine.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -Eeux
+
+declare -A games=(
+    [avp2primal-hunt]="$HOME/.wine/drive_c/Program Files (x86)/Aliens versus Predator 2 - Primal Hunt/PrimalHunt!.exe"
+    [avp2010]="$HOME/.wine-avp2010/drive_c/Program Files (x86)/DODI-Repacks/Aliens vs Predator/AvP_Launcher.exe"
+)
+
+path="${games[$1]}"
+export WINEPREFIX="${path%/drive_c/*}"
+cd "${path%/*}"
+wine "${path##*/}"
