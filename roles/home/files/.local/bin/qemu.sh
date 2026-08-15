daf9ac1cd62a6c01a70d6e756f576ba67f0b9c68
Tamás Tóth <tomjtoth@gmail.com>
2026-08-15 09:56:06 +0300

    faulty vfio-pci passthrough

--- roles/home/files/.local/bin/qemu.sh
+++ roles/home/files/.local/bin/qemu.sh
@@ -1,54 +1,74 @@
 #!/bin/bash
 
+set -Eeu
+
 # as of 28.21.2024 shared folder support and GPU passthrough not working, win11-24H2 installed just fine
 # setting up samba requires limiting it to localhost
+# 13.8.2026 Code:43 on the VM, prolly needs intel_gop
 
-VM_DIR=~/qemu/${VM_OS:-win11}
+VM_DIR="$HOME/.qemu-VMs/${VM_OS:-win11}"
 VM_DISK="${VM_DIR}/disk"
 VM_INPUT="-usb -device usb-tablet"
-VM_VIDEO="-vga qxl"
-VM_VIDEO="-vga vmware"
+# VM_VIDEO="-vga qxl"
+VM_VIDEO="-vga vmware -vnc 127.0.0.1:0"
+
+# dumped host bios: sudo flashrom -p internal -r host_bios.bin
+# uefitool host_bios.bin # CTRL+F IntelGopDriver -> PE32 image -> Extract body -> below file
+INTEL_GOP=~/intel_gop.efi
+
+SUDO=
+
+if [ -n "${VFIO_PCI:-}" ]; then
+    SUDO=sudo
+    VM_VIDEO="
+        -device ramfb -vga none -vnc 0.0.0.0:0
+        -device vfio-pci-igd-lpc-bridge,id=lpc0,bus=pcie.0,addr=1f.0
+        -device vfio-pci,host=$VFIO_PCI,bus=pcie.0,addr=0x2,x-igd-opregion=on,x-igd-gms=4,romfile=$INTEL_GOP
+    "
+fi
+
 VM_AUDIO="-audiodev pipewire,id=snd0 -device ich9-intel-hda"
 
 UEFI_FLAGS="
     -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2/x64/OVMF_CODE.secboot.4m.fd
-    -drive if=pflash,format=raw,file=${VM_DIR}/.OVMF_VARS.4m.fd
+    -drive if=pflash,format=raw,file="${VM_DIR}/.OVMF_VARS.4m.fd"
 "
 
-TPM_DIR=${VM_DIR}/.tpm
+TPM_DIR=$(mktemp -d)
 TPM_FLAGS="
     -chardev socket,id=chrtpm,path=${TPM_DIR}/swtpm-sock
     -tpmdev emulator,id=tpm0,chardev=chrtpm
     -device tpm-tis,tpmdev=tpm0
 "
 
-if [[ "$1" == "install" ]]; then
-    mkdir -p "${TPM_DIR}"
-    cp /usr/share/edk2/x64/OVMF_VARS.4m.fd "${VM_DIR}/.OVMF_VARS.4m.fd"
-    
-    qemu-img create -f qcow2 $VM_DISK 100G
+if [[ "${1:-not}" == "install" ]]; then
+    cp /usr/share/edk2/x64/OVMF_VARS.4m.fd \
+        "${VM_DIR}/.OVMF_VARS.4m.fd"
+
+    qemu-img create -f qcow2 "$VM_DISK" 500G
 
     INSTALL_FLAGS="-nic none -cdrom $2 -boot order=d"
 fi
 
-# starting TPM2.0 emulation for win11
-swtpm socket --tpm2 --tpmstate dir=${TPM_DIR} \
-    --ctrl type=unixio,path=${TPM_DIR}/swtpm-sock &
+$SUDO swtpm socket \
+    --tpm2 \
+    --tpmstate "dir=$TPM_DIR" \
+    --ctrl "type=unixio,path=$TPM_DIR/swtpm-sock" &
 
-TPM_PID=$!
-
-qemu-system-x86_64 \
-    -m 6G -cpu host -smp $(nproc) \
+$SUDO qemu-system-x86_64 \
+    -m 6G \
+    -cpu host,kvm=off \
+    -smp 4 \
     -machine q35 \
-    -drive file=${VM_DISK} \
+    -drive file="$VM_DISK",format=qcow2 \
     -enable-kvm \
     -rtc base=localtime \
     $TPM_FLAGS \
     $UEFI_FLAGS \
     $VM_INPUT \
-    $VM_AUDIO \
+    `# $VM_AUDIO` \
     $VM_VIDEO \
-    $INSTALL_FLAGS
+    ${INSTALL_FLAGS:-}
 
-# swtpm seems to be killed after qemu exits...
-# kill $TPM_PID
+$SUDO pkill swtpm
+rm -rf $TPM_DIR
