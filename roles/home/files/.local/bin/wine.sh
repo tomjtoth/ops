8359198f9d8fb37ef5c62f0e21e6763e8b40be84
Tamás Tóth <tomjtoth@gmail.com>
2026-08-15 12:49:48 +0300

    added XIII and install wrapper to wine.sh

--- roles/home/files/.local/bin/wine.sh
+++ roles/home/files/.local/bin/wine.sh
@@ -4,10 +4,20 @@ set -Eeux
 
 declare -A games=(
     [avp2primal-hunt]="$HOME/.wine/drive_c/Program Files (x86)/Aliens versus Predator 2 - Primal Hunt/PrimalHunt!.exe"
+    
+    [xiii]="$HOME/.wine-xiii/drive_c/GOG Games/XIII/system/XIII.exe"
+
     [avp2010]="$HOME/.wine-avp2010/drive_c/Program Files (x86)/DODI-Repacks/Aliens vs Predator/AvP_Launcher.exe"
 )
 
-path="${games[$1]}"
-export WINEPREFIX="${path%/drive_c/*}"
-cd "${path%/*}"
-wine "${path##*/}"
+case "$1" in 
+    install) WINEPREFIX=~/.wine-$2 wine "$3"; exit 0;;
+
+    *)
+        path="${games[$1]}"
+        export WINEPREFIX="${path%/drive_c/*}"
+        cd "${path%/*}"
+        wine "${path##*/}"
+        ;;
+esac
+
